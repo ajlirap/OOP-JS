52fca26ac34c724e152198f558ce496b32bda10f
ajlirap <ajlirap@gmail.com>
2022-06-02 01:11:17 -0700

    Second Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("***********OBJECT LITERAL SYNTAX *************");
+console.log("Hello World from OOP-JS");
+const circle = {
+  radius: 1,
+  location: {
+    x: 1,
+    y: 1,
+  },
+  draw: function () {
+    console.log("draw");
+  },
+};
+
+circle.draw();
+console.log("***********FACTORY*************");
